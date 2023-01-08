ba249c6bea9837c8f072fd4fec4870b4e6ecb3bc
tanmaysihare <tanmaysihare@gmail.com>
2023-01-08 14:47:03 +0530

    bold and font color done

---
+++ index.js
@@ -0,0 +1,9 @@
+var x= document.getElementById('header-title');
+x.style.border='solid 5px #000';
+var y= document.getElementById('main-header');
+y.style.border='solid 10px #ff7700';
+x.style.color='#ffff00';
+let z= document.getElementsByTagName('h2');
+console.log(z);
+z[0].style.fontWeight='bold';
+z[0].style.color='green';
