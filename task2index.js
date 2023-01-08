02f90d8d64ed508c63af1d02edabfab8aabd181a
tanmaysihare <tanmaysihare@gmail.com>
2023-01-08 15:03:37 +0530

    task4-index

---
+++ task2index.js
@@ -0,0 +1,7 @@
+let x = document.getElementsByClassName('list-group-item');
+console.log(x);
+x[2].style.backgroundColor='green';
+for(let i=0;i<x.length;i++){
+    x[i].style.fontWeight='bold';
+    x[i].style.color='#f00';
+}
