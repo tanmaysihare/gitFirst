2acbbd36f76ee1081be1ce1cb9574677b45823b7
tanmaysihare <tanmaysihare@gmail.com>
2023-01-22 18:32:23 +0530

    expense traker

--- project1.js
+++ project1.js
@@ -1,30 +1,17 @@
-document.getElementById('submt').onmouseover= mover;
-document.getElementById('submt').onmouseout= mout;
-//let nam=document.querySelectorAll('input');
-function mover(){
-    document.querySelector('#submt').value = "Click Me TO Submit";
-}
-function mout(){
-    document.querySelector('#submt').value = 'Get A Call';
-}
+
 function print(event) {
     event.preventDefault();
-   // let multid=[];
    const UserName=event.target.Name.value;
    const UserEmail=event.target.Email.value;
    const userNumber=event.target.Phone.value;
-   const userAppDate=event.target.Date.value;
-   const userAppTime=event.target.Time.value;
-    let userDetail={
+       let userDetail={
         UserName,
         UserEmail,
-        userNumber,
-        userAppDate,
-        userAppTime,};
-    localStorage.setItem(userDetail.UserEmail,JSON.stringify(userDetail));
+        userNumber,};
+    localStorage.setItem(userDetail.userNumber,JSON.stringify(userDetail));
     let dis=document.createElement('li');
     let listt=document.querySelector('#display');
-    dis.textContent =userDetail.UserName+' - '+userDetail.UserEmail+' - '+userDetail.userNumber+' - '+userDetail.userAppDate+' - '+userDetail.userAppTime;
+    dis.textContent =userDetail.UserName+' - '+userDetail.UserEmail+' - '+userDetail.userNumber;
    listt.appendChild(dis);
    let dbtn= document.createElement('button');
    dbtn.className='btn btn-danger btn-sm float-right delete';
@@ -35,24 +22,18 @@ function print(event) {
    edit.className='btn btn-success btn-sm float-right';
    edit.appendChild(document.createTextNode('edit'));
    edit.onclick = () => {
-    localStorage.removeItem(userDetail.UserEmail);
+    localStorage.removeItem(userDetail.userNumber);
     listt.removeChild(dis);
     document.getElementById('name').value = UserName;
     document.getElementById('email').value = UserEmail;
     document.getElementById('phone').value = userNumber;
-    document.getElementById('date').value = userAppDate;
-    document.getElementById('time').value = userAppTime;
-
-   }
+    }
    dis.appendChild(edit);
     function removeItem(e) {
         if(e.target.classList.contains('delete')){
             if(confirm('are you sure ?')){
-               // let listt=document.querySelector('#display');
-              //  let dis = document.querySelector('li');
                 listt.removeChild(dis);
-              //  let x=document.querySelector('#email').value;
-                localStorage.removeItem(userDetail.UserEmail);
+                localStorage.removeItem(userDetail.userNumber);
             }
         }
     }
